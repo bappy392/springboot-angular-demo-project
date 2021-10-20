7ba8e6756ca209e26ca3ce523f8f635cd89bc61b
bappy392 <bappy392@gmail.com>
2021-10-20 21:20:08 +0600

    first commit

---
+++ springboot-backend/src/main/java/com/ussl/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.ussl.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ussl.springboot.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+	
+	
+}
---
+++ springboot-backend/src/test/java/com/ussl/springboot/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ussl.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
